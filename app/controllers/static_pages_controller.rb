94991723e20d8827184b0a5ea94a6f204311981c
Alexandre Ouellette <alexandre.f.ouellette@gmail.com>
2016-06-07 13:52:15 -0400

    Added contact form and SendGrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,4 +9,16 @@ class StaticPagesController < ApplicationController
   def about
     @products = Product.limit(3).order('id desc')
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+
+    ActionMailer::Base.mail(
+      :from => @email,
+      :to => 'alexandre.f.ouellette@gmail.com',
+      :subject => "A new contact message from #{@name}",
+      :body => @message).deliver_later
+  end
 end
