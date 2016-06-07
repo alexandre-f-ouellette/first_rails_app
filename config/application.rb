6c29e499aaa2cddd52ba17b622878f3150de10a7
Alexandre Ouellette <alexandre.f.ouellette@gmail.com>
2016-06-07 17:53:07 -0400

    Fifth attempt

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module Nameofapp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_mailer.default_url_options = { host: 'bikeottawarailsdemo.herokuapp.com' }
   end
 end
