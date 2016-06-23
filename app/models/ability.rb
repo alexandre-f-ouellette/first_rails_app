720a4b76298373bdc308a9d08601161b839d75e2
Alexandre Ouellette <alexandre.f.ouellette@gmail.com>
2016-06-23 17:04:57 -0400

    Added admin role to users and created and secured the destroy comments functionality

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,5 +5,11 @@ class Ability
     user ||= User.new
     can :manage, User, id: user.id
     can :manage, Order, user_id: user.id
+
+    if user.admin?
+      can :delete, Comment
+    else
+      can :read, Comment
+    end
   end
 end
