0eeaa3e35716b4cdf19fca704d36ea9441ead4af
Alexandre Ouellette <alexandre.f.ouellette@gmail.com>
2016-07-26 13:14:31 -0400

    Finished upgrade to rails 5 and implemented basic ActionCable functionality

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#about'
 
+  mount ActionCable.server => '/cable'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
